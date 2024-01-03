daf0d8a465394ae1fbd2ccc08834b4ec6b14a846
anarka312 <natalie.talas@gmail.com>
2024-01-03 23:55:32 +0600

    add js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const infoBtns = document.querySelectorAll('.info-dot');
+const infoHints = document.querySelectorAll('.info-hint');
+
+for (let btn of infoBtns) {
+    btn.addEventListener('click', showHint);
+}
+
+function showHint (e){
+    e.stopPropagation();
+    this.parentNode.querySelector('.info-hint').classList.toggle('none')
+}
+
+document.addEventListener('click', closeHints);
+
+
+function closeHints() {
+    for (let hint of infoHints) {
+        hint.classList.add('.none');
+    }
+}
