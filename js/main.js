bd544b408e65d442d4c10f80a4bc031801d035b1
anarka312 <natalie.talas@gmail.com>
2024-01-12 23:38:23 +0600

    add swiper

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
-const infoBtns = document.querySelectorAll('.info-dot');
-const infoHints = document.querySelectorAll('.info-hint');
+const infoBtns = document.querySelectorAll(".info-dot");
+const infoHints = document.querySelectorAll(".info-hint");
 
 for (let btn of infoBtns) {
-    btn.addEventListener('click', showHint);
+  btn.addEventListener("click", showHint);
 }
 
-function showHint (e){
-    e.stopPropagation();
-    this.parentNode.querySelector('.info-hint').classList.toggle('none')
+function showHint(e) {
+  e.stopPropagation();
+  this.parentNode.querySelector(".info-hint").classList.toggle("none");
 }
 
-document.addEventListener('click', closeHints);
-
+document.addEventListener("click", closeHints);
 
 function closeHints() {
-    for (let hint of infoHints) {
-        hint.classList.add('none');
-    }
+  for (let hint of infoHints) {
+    hint.classList.add("none");
+  }
 }
 
 for (let hint of infoHints) {
-    hint.addEventListener('click', (e) => e.stopPropagation());
+  hint.addEventListener("click", (e) => e.stopPropagation());
 }
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: 'horizantal',
+  // loop: true,
+
+  sliderPerView: 4,
+  spaceBetween: 42,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#sliderNext",
+    prevEl: "#sliderPrev",
+  },
+});
